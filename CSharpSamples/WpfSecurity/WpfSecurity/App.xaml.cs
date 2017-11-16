afa8d0c483437b5255e261366798dd688451eb39
Kolodiazhnyi Serhii <elkrot@gmail.com>
2017-11-16 11:07:01 +0200

    Add Auth Project

---
+++ CSharpSamples/WpfSecurity/WpfSecurity/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfSecurity
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //Create a custom principal with an anonymous identity at startup
+            CustomPrincipal customPrincipal = new CustomPrincipal();
+            AppDomain.CurrentDomain.SetThreadPrincipal(customPrincipal);
+
+            base.OnStartup(e);
+
+            //Show the login view
+            AuthenticationViewModel viewModel = new AuthenticationViewModel(new AuthenticationService());
+            IView loginWindow = new LoginWindow(viewModel);
+            loginWindow.Show();
+        }
+    }
+}
